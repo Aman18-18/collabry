4c179e1cb84e743069a02c5cb490f134bfc7e729
Aman18-18 <amanssingh1827@gmail.com>
2026-08-16 21:44:11 +0530

    initialising authentication process

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+function AuthProvider({children}){
+    const [user, setUser] = useState(()=>{
+    
+        const savedUser = localStorage.getItem("user");
+
+        return savedUser ? JSON.parse(savedUser) : null;
+       });
+
+       function login(userData) {
+        setUser(userData);
+        localStorage.setItem("user",JSON.stringify(userData));
+    }
+       function logout() {
+        setUser(null);
+        localStorage.removeItem("user");
+    }
+    return(
+   <AuthContext.Provider value={{
+     user,
+     login,
+     logout
+
+   }
+   
+   }>
+      {children}
+   </AuthContext.Provider>
+);
+}
+
+export default AuthProvider;
+
+
